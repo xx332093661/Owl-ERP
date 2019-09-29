3984f183b53a12b785b8acbc953186c63798b4dd
吴双 <631368@qq.com>
2019-09-29 11:34:02 +0800

    depends modify

--- myaddons/cj_api/__manifest__.py
+++ myaddons/cj_api/__manifest__.py
@@ -14,19 +14,15 @@
 
     'data': [
         'data/res_config_setting.xml',
-        'data/cron.xml',
         'data/uom_data.xml',
-
-
         'security/api_security.xml',
         'security/ir.model.access.csv',
-
         'views/api_message_view.xml',
         'views/cj_org_view.xml',
         'views/res_config_settings_views.xml',
         'views/cj_oa_api_view.xml',
-
         'views/menu_view.xml',
+        'data/cron.xml',
 
 
 
