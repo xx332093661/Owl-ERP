8b309f111613fbf12027a3f906dea56536c8489f
xichun <675938238@qq.com>
2019-10-18 17:30:54 +0800

    POS盘点接口

---
+++ myaddons/cj_api/controllers/pos_interface.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+# 与POS系统临时接口
+###################################################################################
+import importlib
+import json
+import logging
+import traceback
+
+from odoo import http
+from odoo.tools import config
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class PosInterface(http.Controller):
+    @http.route('/pos/inventory', type='json', auth="none", methods=['POST'], csrf=False)
+    def pos_inventory(self):
+        """pos盘点接口
+        数据结构
+        传入参数：{
+            'username': 用户名
+            'password': 密码
+            'data': [{
+                'store_code': 门店编码,
+                'store_name': 门店名称
+                'pos_id': pos盘点单ID
+                'lines': [{
+                    'default_code': 物料编码
+                    'goods_name': 商品名称
+                    'product_qty': 在手数量
+                    'unit_cost': 单位成本
+                }]  盘点明细
+            }] 盘点数据
+        }
+        返回结果：{
+             'state': 1 处理状态(1-成功, 0-失败),
+             'msg': 错误信息
+        }
+        """
+        module = importlib.import_module('odoo.addons.cj_api.models.api_message')
+        my_validation_error = module.MyValidationError
+        errors = module.PROCESS_ERROR
+
+        db = config['db_name']
+        username = request.jsonrequest.get('username')
+        password = request.jsonrequest.get('password')
+
+        uid = request.session.authenticate(db, username, password)
+        if not uid:
+            return {
+                'state': 0,
+                'msg': '用户名或密码错误！'
+            }
+
+        try:
+            inventory_data = json.loads(request.jsonrequest.get('data') or '[]')
+        except ValueError:
+            return {
+                'state': 0,
+                'msg': '处理盘点数据出错，请传json格式字符串！'
+            }
+
+        if not inventory_data:
+            return {
+                'state': 0,
+                'msg': '没有盘点数据！'
+            }
+
+        body = []
+        for data in inventory_data:
+            store_code = data.get('store_code')
+            store_name = data.get('store_name')
+            if not store_code:
+                return {
+                    'state': 0,
+                    'msg': '请传门店编码！'
+                }
+            lines = data.get('lines')
+            if not lines:
+                return {
+                    'state': 0,
+                    'msg': '请传递门店%s的盘点明细！'
+                }
+
+            for line in lines:
+                default_code = line.get('default_code')
+                if not default_code:
+                    return {
+                        'state': 0,
+                        'msg': '物料编码不能为空！'
+                    }
+                product_qty = line.get('product_qty')
+
+                body.append({
+                    "quantity": product_qty,
+                    "storeName": store_name,
+                    "updateTime": '',
+                    "goodsCode": default_code,
+                    "storeCode": store_code
+                })
+
+        try:
+            request.env['api.message'].deal_mustang_to_erp_store_stock_push(json.dumps(body))
+        except my_validation_error as e:
+            error_no = e.error_no
+            error_msg = errors[error_no]
+            return {
+                'state': 0,
+                'msg': error_msg
+            }
+        except Exception:
+            _logger.error(traceback.format_exc())
+            return {
+                'state': 0,
+                'msg': '处理盘点数据发生未知错误'
+            }
+
+        return {
+            'state': 1,
+            'msg': ''
+        }
+
+
+
+
+
