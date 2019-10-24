95f066681474b7b57bbd7c12ea4ef45022ddf1a2
xichun <675938238@qq.com>
2019-10-24 18:13:50 +0800

    供应商报价单修改

--- myaddons/cj_purchase/models/purchase_price_list.py
+++ myaddons/cj_purchase/models/purchase_price_list.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api
 from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT, \
-    DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
-from odoo.tools.float_utils import float_is_zero, float_compare
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
-import logging
-import json
-
-_logger = logging.getLogger(__name__)
+READONLY_STATES = {
+    'draft': [('readonly', False)]
+}
 
 
 class PurchasePriceList(models.Model):
-    """报价单"""
-
     _name = 'purchase.price.list'
     _inherit = ['mail.thread']
-    _description = u'报价单'
+    _description = '报价单'
     _order = 'id desc'
 
-    company_id = fields.Many2one('res.company', '公司', default=lambda self: self.env.user.company_id)
-    name = fields.Char('标题')
-    order_time = fields.Datetime('报价时间', default=lambda self: datetime.now().strftime(DATETIME_FORMAT))
-    supplierinfo_ids = fields.One2many('product.supplierinfo', 'price_list_id', '供应商价格表')
-    paper = fields.Binary('纸质文件')
+    company_id = fields.Many2one('res.company', '公司', default=lambda self: self.env.user.company_id, readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    name = fields.Char('标题', readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    order_time = fields.Datetime('报价时间', default=lambda self: datetime.now().strftime(DATETIME_FORMAT), readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    supplierinfo_ids = fields.One2many('product.supplierinfo', 'price_list_id', '供应商价格表', readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    paper = fields.Binary('纸质文件', readonly=1, states=READONLY_STATES, track_visibility='onchange')
+
+    state = fields.Selection([('draft', '草稿'), ('confirm', '确认'), ('purchase_manager_confirm', '采购经理审核'), ('done', '财务经理审核')], track_visibility='onchange')
 
 
 class ProductSupplierinfo(models.Model):
