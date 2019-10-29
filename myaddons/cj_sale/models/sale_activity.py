6240990968ec9efa9575fb7aa7e31edd693c072c
xichun <675938238@qq.com>
2019-10-29 15:09:47 +0800

    营销活动修改

--- myaddons/cj_sale/models/sale_activity.py
+++ myaddons/cj_sale/models/sale_activity.py
@@ -1,75 +1,108 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api
-import logging
+from odoo.exceptions import ValidationError
 
-_logger = logging.getLogger(__name__)
 
-#draft,confirm,sale_manager_confirm,account_manager_confirm,done
 STATUS = [
     ('draft', '草稿'),
     ('confirm', '确认'),
-    ('sale_manager_confirm', '销售经理确认'),
-    ('account_manager_confirm', '财务经理确认'),
-    ('done', '完成'),
-    ('cancle', '取消')
+    ('sale_manager_confirm', '销售经理审核'),
+    ('done', '财务经理审批'),
+    ('cancel', '取消')
 ]
+
+READONLY_STATES = {
+    'draft': [('readonly', False)],
+}
+
 
 class SaleActivity(models.Model):
     _name = 'cj.sale.activity'
     _description = '营销活动'
     _order = 'id desc'
-    _rec_name = 'name'
+    _inherit = ['mail.thread']
 
-    active = fields.Boolean(string='有效', default=False)
-    name = fields.Char(string='活动名称', required=True, index=True)
-    principal = fields.Many2one(comodel_name='res.users', string='发起人')
-    description = fields.Text(string='活动描述')
-    code = fields.Char(string='活动编号', required=True, index=True, copy=False)
-    start_time = fields.Datetime(string='开始时间', default=fields.datetime.now())
-    end_time = fields.Datetime(string='结束时间')
-    channels_ids = fields.Many2many('sale.channels', string='营销渠道')
-    company_id = fields.Many2one('res.company', string='公司', default=lambda self: self.env.user.company_id.id)
-    line_ids = fields.One2many("cj.sale.activity.line", 'activity_id', "活动行")
-    flow_id = fields.Char(string='OA FlowID')
-    state = fields.Selection(selection=STATUS, default='draft')
+    name = fields.Char(string='活动名称', required=True, index=True, readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    code = fields.Char(string='活动编号', required=True, index=True, copy=False, readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    company_id = fields.Many2one('res.company', string='公司', default=lambda self: self.env.user.company_id.id, readonly=1, states=READONLY_STATES, track_visibility='onchange', domain=lambda self: [('id', 'child_of', [self.env.user.company_id.id])])
+    principal = fields.Many2one(comodel_name='res.users', string='发起人', default=lambda self: self.env.user.id, readonly=1, states=READONLY_STATES, track_visibility='onchange', domain=lambda self: [('company_id', 'child_of', [self.env.user.company_id.id])])
+    description = fields.Text(string='活动描述', readonly=1, states=READONLY_STATES, track_visibility='onchange')
 
+    start_time = fields.Datetime(string='开始时间', default=fields.datetime.now(), readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    end_time = fields.Datetime(string='结束时间', readonly=1, states=READONLY_STATES, track_visibility='onchange')
+
+    channels_ids = fields.Many2many('sale.channels', string='营销渠道', readonly=1, states=READONLY_STATES, track_visibility='onchange')
+
+    line_ids = fields.One2many("cj.sale.activity.line", 'activity_id', "活动行", readonly=1, states=READONLY_STATES, track_visibility='onchange')
+    # flow_id = fields.Char(string='OA FlowID')
+    state = fields.Selection(selection=STATUS, default='draft', track_visibility='onchange', string='状态')
+
+    active = fields.Boolean(string='有效', default=True)
+
+    @api.multi
     def action_confirm(self):
-        self.write({'active': False})
-        self.write({'state': 'confirm'})
+        """销售专员确认"""
+        if self.state != 'draft':
+            raise ValidationError('只有草稿单据才能确认！')
 
-    def action_sale_user_refuse(self):
-        self.write({'active': False})
-        self.write({'state': 'draft'})
+        if not self.line_ids:
+            raise ValidationError('请输入活动明细！')
 
-    def action_sale_manager_confirm(self):
-        self.write({'active': False})
-        self.write({'state': 'sale_manager_confirm'})
+        self.state = 'confirm'
 
-    def action_sale_user_confirm(self):
-        self.write({'active': True})
-        self.write({'state': 'account_manager_confirm'})
-
-    def action_cancel(self):
-        self.write({'active': False})
-        self.write({'state': 'cancel'})
-
+    @api.multi
     def action_draft(self):
-        self.write({'active': False})
-        self.write({'state': 'draft'})
+        """设为草稿"""
+        if self.state not in ['confirm', 'cancel']:
+            raise ValidationError('只有确认或取消的单据才能重置为草稿！')
 
+        self.state = 'draft'
+
+    @api.multi
+    def action_cancel(self):
+        """取消活动"""
+        if self.state not in ['confirm', 'draft']:
+            raise ValidationError('只有确认或草稿的单据才能取消！')
+
+        self.state = 'cancel'
+
+    @api.multi
+    def action_sale_manager_confirm(self):
+        """销售经理确认"""
+        if self.state != 'confirm':
+            raise ValidationError('只有确认的单据才能由销售经理审核！')
+
+        self.state = 'sale_manager_confirm'
+
+    @api.multi
+    def action_finance_manager_confirm(self):
+        """财务经理审核"""
+        if self.state != 'sale_manager_confirm':
+            raise ValidationError('只有销售经理审核的单据才能由财务经理审批！')
+
+        self.state = 'done'
+
+    @api.multi
+    def unlink(self):
+        if any([res.state != 'draft' for res in self]):
+            raise ValidationError('非草稿状态单据不能删除！')
+
+        return super(SaleActivity, self).unlink()
 
 
 class SaleActivityLine(models.Model):
-
     _name = 'cj.sale.activity.line'
     _description = '活动行'
 
-    product_id = fields.Many2one('product.product', string='商品')
-    unit_price = fields.Float("最低单价")
-    product_qty = fields.Integer("总限制数量")
-    activity_id = fields.Many2one("cj.sale.activity", default=lambda self: self.env.context.get('active_id'), required=True, index=True, ondelete='cascade')
-    used_qty = fields.Integer("使用数量", default=0)
+    activity_id = fields.Many2one("cj.sale.activity", required=True, index=True, ondelete='cascade')
+    product_id = fields.Many2one('product.product', string='商品', required=1)
+    unit_price = fields.Float("最低单价", required=1)
+    product_qty = fields.Integer("总限制数量", required=1)
+
+    used_qty = fields.Integer("使用数量", compute='_compute_used_qty')
     order_limit_qty = fields.Integer("每单订单限量")
 
-
-
+    @api.multi
+    def _compute_used_qty(self):
+        """计算已使用量"""
+        # TODO 待处理
