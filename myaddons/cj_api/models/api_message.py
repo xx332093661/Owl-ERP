095f8a4805eda9875705966d1db11026d2411a32
xichun <675938238@qq.com>
2019-12-07 16:11:57 +0800

    付款申请修改

--- myaddons/cj_api/models/api_message.py
+++ myaddons/cj_api/models/api_message.py
@@ -1009,7 +1009,7 @@ class ApiMessage(models.Model):
                 return content['storeCode']
             if channel_code == 'enomatic':  # 销售渠道为售酒机，则销售主体是02014(四川省川酒集团信息科技有限公司)
                 return '02014'
-            if channel_code in ['jd', 'tmall', 'taobao', 'jxw']:  # 线上渠道，销售主体默认为02020（泸州电子商务发展有限责任公司）
+            if channel_code in ['jd', 'tmall', 'taobao', 'jxw']:  # 线上渠道，销售主体默认为02020（泸州电子商务发展有限责任公司）jxw: 酒仙网
                 return '02020'
 
             return content['storeCode']
