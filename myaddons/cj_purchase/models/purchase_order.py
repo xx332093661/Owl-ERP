857f80573958caa560ce1b04fd2a8eb90230e88f
xichun <675938238@qq.com>
2019-12-10 19:39:38 +0800

    api接口修改

--- myaddons/cj_purchase/models/purchase_order.py
+++ myaddons/cj_purchase/models/purchase_order.py
@@ -514,8 +514,8 @@ class PurchaseOrder(models.Model):
     def action_manager_approval(self):
         """因为提交OA审批等待时间太久，可由系统管理员角色直接审批，而无需OA审批"""
         self.ensure_one()
-        if self.state != 'confirm':
-            raise ValidationError('只有审核的单据才可以由管理员审批！')
+        if self.state != 'oa_sent':
+            raise ValidationError('只有提交审批的单据才可以由管理员审批！')
 
         self.state = 'oa_accept'  # 审批通过
 
