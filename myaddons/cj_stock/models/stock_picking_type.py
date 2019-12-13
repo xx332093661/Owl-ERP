d19d0b43f3f9a89480402c5dbd655f31ff9d88fa
xichun <675938238@qq.com>
2019-12-13 14:42:50 +0800

    仓库概览修改

--- myaddons/cj_stock/models/stock_picking_type.py
+++ myaddons/cj_stock/models/stock_picking_type.py
@@ -10,17 +10,17 @@ class StockPickingType(models.Model):
     """
     _inherit = 'stock.picking.type'
 
-    @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        """
-        仓库动作控制台，仓库专员和仓库经理，只能访问仓管员或仓库经理字段为当前用户的仓库对应的操作
-        base.group_erp_manager用户可访问当前公司及所有子公司的仓库对应的操作
-        """
-        if 'only_myself' in self._context and not self.env.user.has_group('base.group_erp_manager'):
-            warehouse_ids = self.env['stock.warehouse'].search([]).ids
-
-            args = args or []
-            args.append(('warehouse_id', 'in', warehouse_ids))
-
-        return super(StockPickingType, self)._search(args, offset=offset, limit=limit, order=order,
-                                                     count=False, access_rights_uid=access_rights_uid)
+    # @api.model
+    # def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    #     """
+    #     仓库动作控制台，仓库专员和仓库经理，只能访问仓管员或仓库经理字段为当前用户的仓库对应的操作
+    #     base.group_erp_manager用户可访问当前公司及所有子公司的仓库对应的操作
+    #     """
+    #     if 'only_myself' in self._context and not self.env.user.has_group('base.group_erp_manager'):
+    #         warehouse_ids = self.env['stock.warehouse'].search([]).ids
+    #
+    #         args = args or []
+    #         args.append(('warehouse_id', 'in', warehouse_ids))
+    #
+    #     return super(StockPickingType, self)._search(args, offset=offset, limit=limit, order=order,
+    #                                                  count=False, access_rights_uid=access_rights_uid)
