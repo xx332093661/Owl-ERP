a3dab82ed57fc6b3041d82e78daab02efa763945
liuchang <332093661@qq.com>
2020-01-10 16:14:40 +0800

    库存检测调整：每天凌晨定时运行，读取前一日的实时库存数据， 同一商品同一仓库重复的保留时间最新的一条，形成检测的数据列表。 对这个检测数据列表的库存与前一天23:59:59秒的库存进行比对， 如有差异记录到： 库存差异记录表

--- myaddons/cj_stock/models/stock_check_record.py
+++ myaddons/cj_stock/models/stock_check_record.py
@@ -2,6 +2,51 @@
 from odoo.tools import float_compare
 from odoo import models, api, fields
 from odoo.exceptions import ValidationError
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from datetime import datetime, timedelta
+
+
+class StockCheckGoods(models.Model):
+    _name = 'stock.check.goods'
+    _description = '商品库存检查'
+    _order = 'id desc'
+
+    product_id = fields.Many2one('product.product', '商品')
+    message_id = fields.Many2one('api.message', '消息')
+    warehouse_id = fields.Many2one('stock.warehouse', '仓库')
+    check_time = fields.Datetime('检查时间')
+    check_date = fields.Date('检查日期')
+    zt_qty = fields.Float('中台库存')
+    state = fields.Selection([('draft', '未检测'), ('done', '完成'), ('diff', '有差异')], '状态', default='draft')
+    check_record_id = fields.Many2one('stock.check.record', '检查记录')
+
+    @api.model
+    def create_stock_check_goods(self, check_time, product, warehouse, zt_qty, message_id=None):
+        """创建实时库存差异"""
+
+        check_date = (datetime.strptime(check_time, DATETIME_FORMAT) + timedelta(hours=8)).strftime(DATE_FORMAT)
+
+        check_goods = self.search([('product_id', '=', product.id),
+                                   ('warehouse_id', '=', warehouse.id),
+                                   ('check_date', '=', check_date),
+                                   ('check_time', '<=', check_time)], limit=1)
+        if not check_goods:
+            self.create({
+                'product_id': product.id,
+                'message_id': message_id,
+                'check_time': check_time,
+                'check_date': check_date,
+                'warehouse_id': warehouse.id,
+                'zt_qty': zt_qty,
+            })
+        else:
+            self.write({
+                'message_id': message_id,
+                'check_time': check_time,
+                'zt_qty': zt_qty,
+                'state': 'draft'
+            })
 
 
 class StockCheckRecord(models.Model):
@@ -10,30 +55,48 @@ class StockCheckRecord(models.Model):
     _order = 'id desc'
 
     product_id = fields.Many2one('product.product', '商品')
-    message_id = fields.Many2one('api.message', '消息')
+    # message_id = fields.Many2one('api.message', '消息')
     check_time = fields.Datetime('检查时间')
     warehouse_id = fields.Many2one('stock.warehouse', '仓库')
     zt_qty = fields.Float('中台库存')
     erp_qty = fields.Float('erp库存')
 
     @api.model
-    def create_stock_check_record(self, check_time, product, warehouse, zt_qty, message_id=None):
+    def check_stock_qty_cron(self):
+        check_goods_obj = self.env['stock.check.goods']
+
+        check_goods = check_goods_obj.search([('state', '=', 'draft')])
+
+        for cg in check_goods:
+            check_record_id = self.create_stock_check_record(cg.check_date, cg.product_id, cg.warehouse_id, cg.zt_qty)
+            if check_record_id:
+                cg.write({
+                    'check_record_id': check_record_id,
+                    'state': 'diff'
+                })
+            else:
+                cg.state = 'done'
+
+    @api.model
+    def create_stock_check_record(self, check_date, product, warehouse, zt_qty):
         """创建实时库存差异"""
+        to_date = check_date.strftime(DATE_FORMAT) + ' 15:59:59'
+
         context = {
             # 'lot_id': warehouse.lot_stock_id.id,
             'location': warehouse.lot_stock_id.id,
-            'to_date': check_time,
+            'to_date': to_date,
         }
         res = product.sudo().with_context(**context)._product_available()
 
         qty_available = res[product.id]['qty_available']
 
         if float_compare(zt_qty, qty_available, precision_rounding=0.01) != 0:
-            self.create({
+            res = self.create({
                 'product_id': product.id,
-                'message_id': message_id,
-                'check_time': check_time,
+                'check_time': to_date,
                 'warehouse_id': warehouse.id,
                 'zt_qty': zt_qty,
                 'erp_qty': qty_available
             })
+            return res.id
