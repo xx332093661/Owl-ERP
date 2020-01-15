0bff6a655a6cec2e34e4f8b064b20aed617f272e
xichun <675938238@qq.com>
2020-01-15 17:01:30 +0800

    仓库概览修改

--- myaddons/cj_stock/models/stock_picking_type.py
+++ myaddons/cj_stock/models/stock_picking_type.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, api
 
 
 class StockPickingType(models.Model):
-    """
-    主要功能
-        仓库动作控制台，仓库专员和仓库经理，只能访问仓管员或仓库经理字段为当前用户的仓库对应的操作；
-        base.group_erp_manager用户可访问当前公司及所有子公司的仓库对应的操作
-    """
     _inherit = 'stock.picking.type'
-
-    def _search_count_picking_ready(self, operator, value):
-
-        data = self.env['stock.picking'].read_group([('state', '=', 'assigned')], ['picking_type_id'], ['picking_type_id'])
-        count = {x['picking_type_id'][0]: x['picking_type_id_count'] for x in data if x['picking_type_id']}
-
-
-        return []
 
     # @api.model
     # def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
@@ -39,19 +26,3 @@ class StockPickingType(models.Model):
             domain.append(('id', 'in', ids))
 
         return super(StockPickingType, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
-
-
-    # @api.model
-    # def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-    #     """
-    #     仓库动作控制台，仓库专员和仓库经理，只能访问仓管员或仓库经理字段为当前用户的仓库对应的操作
-    #     base.group_erp_manager用户可访问当前公司及所有子公司的仓库对应的操作
-    #     """
-    #     if 'only_myself' in self._context and not self.env.user.has_group('base.group_erp_manager'):
-    #         warehouse_ids = self.env['stock.warehouse'].search([]).ids
-    #
-    #         args = args or []
-    #         args.append(('warehouse_id', 'in', warehouse_ids))
-    #
-    #     return super(StockPickingType, self)._search(args, offset=offset, limit=limit, order=order,
-    #                                                  count=False, access_rights_uid=access_rights_uid)
